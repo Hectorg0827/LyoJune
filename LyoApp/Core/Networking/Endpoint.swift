6db2ab704c2db8550a902c8c18dbb509354c853f
Your Name <hector.garcia0827@gmail.com>
2025-06-25 19:21:00 -0400

    Update Endpoint.swift

--- LyoApp/Core/Networking/Endpoint.swift
+++ LyoApp/Core/Networking/Endpoint.swift
@@ -1,31 +1,58 @@
 
 import Foundation
 
+// MARK: - Endpoint
 public struct Endpoint {
     let path: String
     var method: HTTPMethod = .get
     var body: Encodable? = nil
+    var headers: [String: String] = [:]
+    var queryParameters: [String: String] = [:]
+    
+    public init(path: String, method: HTTPMethod = .get, body: Encodable? = nil, headers: [String: String] = [:], queryParameters: [String: String] = [:]) {
+        self.path = path
+        self.method = method
+        self.body = body
+        self.headers = headers
+        self.queryParameters = queryParameters
+    }
 
     func asURLRequest() throws -> URLRequest {
-        guard let url = URL(string: "https://api.lyo.app/v1" + path) else {
+        let baseURL = ConfigurationManager.shared.apiBaseURL
+        guard let url = URL(string: baseURL + path) else {
+            throw APIError.invalidURL
+        }
+        
+        // Add query parameters
+        var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)
+        if !queryParameters.isEmpty {
+            urlComponents?.queryItems = queryParameters.map { URLQueryItem(name: $0.key, value: $0.value) }
+        }
+        
+        guard let finalURL = urlComponents?.url else {
             throw APIError.invalidURL
         }
 
-        var request = URLRequest(url: url)
+        var request = URLRequest(url: finalURL)
         request.httpMethod = method.rawValue
+        
+        // Set default headers
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // Add custom headers
+        for (key, value) in headers {
+            request.setValue(value, forHTTPHeaderField: key)
+        }
 
+        // Add body if present
         if let body = body {
-            request.httpBody = try JSONEncoder().encode(body)
+            do {
+                request.httpBody = try JSONEncoder().encode(body)
+            } catch {
+                throw APIError.encodingError
+            }
         }
 
         return request
     }
-}
-
-enum APIError: Error {
-    case invalidURL
-    case networkError
-    case decodingError
-    case mockDataNotFound
 }
