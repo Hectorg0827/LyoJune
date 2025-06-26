7bf0ddba4ba50d7074a55a7035259b8fae55961e
Your Name <hector.garcia0827@gmail.com>
2025-06-26 18:00:42 -0400

    Create AnyCodable.swift

---
+++ LyoApp/Core/Utilities/AnyCodable.swift
@@ -0,0 +1,57 @@
+import Foundation
+
+// MARK: - AnyCodable Helper
+/// A type-erased Codable wrapper for handling Any types in JSON
+public struct AnyCodable: Codable {
+    public let value: Any
+    
+    public init(_ value: Any) {
+        self.value = value
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        
+        if let bool = try? container.decode(Bool.self) {
+            value = bool
+        } else if let int = try? container.decode(Int.self) {
+            value = int
+        } else if let double = try? container.decode(Double.self) {
+            value = double
+        } else if let string = try? container.decode(String.self) {
+            value = string
+        } else if let array = try? container.decode([AnyCodable].self) {
+            value = array.map { $0.value }
+        } else if let dict = try? container.decode([String: AnyCodable].self) {
+            value = dict.mapValues { $0.value }
+        } else {
+            throw DecodingError.dataCorruptedError(in: container, debugDescription: "Unsupported type")
+        }
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        
+        switch value {
+        case let bool as Bool:
+            try container.encode(bool)
+        case let int as Int:
+            try container.encode(int)
+        case let double as Double:
+            try container.encode(double)
+        case let string as String:
+            try container.encode(string)
+        case let array as [Any]:
+            let codableArray = array.map { AnyCodable($0) }
+            try container.encode(codableArray)
+        case let dict as [String: Any]:
+            let codableDict = dict.mapValues { AnyCodable($0) }
+            try container.encode(codableDict)
+        default:
+            throw EncodingError.invalidValue(value, EncodingError.Context(
+                codingPath: container.codingPath,
+                debugDescription: "Unsupported type for encoding"
+            ))
+        }
+    }
+}
