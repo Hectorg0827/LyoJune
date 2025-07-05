a0bddbec5116c0c37b31840b26c010d6ba1fdd71
Your Name <hector.garcia0827@gmail.com>
2025-07-05 14:07:40 -0400

    Update APIService.swift

--- LyoApp/Core/Services/APIService.swift
+++ LyoApp/Core/Services/APIService.swift
@@ -10,9 +10,7 @@ protocol APIService {
 class BaseAPIService: APIService {
     let apiClient: APIClientProtocol
     
-    init(apiClient: APIClientProtocol = {
-        return ConfigurationManager.shared.shouldUseMockBackend ? MockAPIClient.shared : APIClient.shared
-    }()) {
+    init(apiClient: APIClientProtocol = APIClient.shared) {
         self.apiClient = apiClient
     }
 }
@@ -23,16 +21,14 @@ class ServiceFactory {
     
     private init() {}
     
-    private let apiClient: APIClientProtocol = {
-        return ConfigurationManager.shared.shouldUseMockBackend ? MockAPIClient.shared : APIClient.shared
-    }()
+    private let apiClient: APIClientProtocol = APIClient.shared
     
     // Service instances
     lazy var authService = EnhancedAuthService.shared
     lazy var learningService = LearningAPIService(apiClient: apiClient)
     lazy var gamificationService = GamificationAPIService(apiClient: apiClient)
     lazy var communityService = CommunityAPIService(apiClient: apiClient)
-    lazy var userService = UserAPIService(apiClient: apiClient)
+    lazy var userService = CDUserAPIService(apiClient: apiClient)
     lazy var searchService = SearchAPIService(apiClient: apiClient)
     lazy var storiesService = StoriesAPIService(apiClient: apiClient)
     lazy var messagesService = MessagesAPIService(apiClient: apiClient)
